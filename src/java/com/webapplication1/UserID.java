6bc493340494a1830749a5ede11703dffb150e4d
Jevison7x <jevison7x@yahoo.com>
2023-02-05 18:56:38 +0100

    Demonstrating how to get request parameters.

--- src/java/com/webapplication1/UserID.java
+++ src/java/com/webapplication1/UserID.java
@@ -33,12 +33,12 @@ public class UserID extends HttpServlet
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.setContentType("text/html;charset=UTF-8");
         try( PrintWriter out = response.getWriter())
         {
+            String firstName = request.getParameter("firstName");
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
